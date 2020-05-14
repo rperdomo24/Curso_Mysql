99135c8571320b624bd3334295abc430df637bae
Roberto Perdomo <rperdomo32@gmail.com>
2020-05-13 21:39:18 -0600

    mejoras para priemros ejercicior

--- Ejercicios/Parte1.sql
+++ Ejercicios/Parte1.sql
@@ -2,19 +2,19 @@
 -- Obtener todos los libros escritos por autores que cuenten con un seudónimo.
 SELECT *
 from libros
-Where autor_id = (select autor_id FROM autores where seudonimo IS NOT NULL);
+Where autor_id IN (select autor_id FROM autores where seudonimo IS NOT NULL);
 
 -- Obtener el título de todos los libros publicados en el año actual cuyos autores poseen un pseudónimo.
 SELECT titulo
 FROM libros
-WHERE autor_id = (SELECT autor_id FROM autores where seudonimo IS NOT NULL)
+WHERE autor_id IN (SELECT autor_id FROM autores where seudonimo IS NOT NULL)
 AND DAYOFYEAR(fecha_creacion) = DAYOFYEAR(NOW());
 
 -- Obtener todos los libros escritos por autores que cuenten con un seudónimo 
 -- y que hayan nacido ante de 1965.
 SELECT *
 from libros
-Where autor_id = (
+Where autor_id IN (
     select autor_id 
     FROM autores 
     where seudonimo IS NOT NULL
